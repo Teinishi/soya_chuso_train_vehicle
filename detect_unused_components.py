ab1d0bb0f4c0c261abd86e9747d8892b962a8247
Teinishi <79356047+Teinishi@users.noreply.github.com>
2025-09-26 13:55:10 +0900

    コンポーネントModの更新も検知して取り込み、未使用のコンポーネントは削除

--- detect_unused_components.py
+++ detect_unused_components.py
@@ -3,27 +3,38 @@ from pathlib import Path
 import os
 from xml.etree import ElementTree as ET
 
-parser = argparse.ArgumentParser()
-parser.add_argument("target", type=str)
-parser.add_argument("-r", "--remove-file", action="store_true")
-args = parser.parse_args()
 
-target = Path(args.target)
-tree = ET.parse(target)
-root = tree.getroot()
+def detect_unused_components(target: Path, remove_file: bool = False, verbose: bool = False) -> set[str]:
+    tree = ET.parse(target)
+    root = tree.getroot()
 
-used_components = set()
+    used_components: set[str] = set()
+    unused_components: set[str] = set()
 
-for c in root.findall("./bodies/body/components/c"):
-    used_components.add(c.get("d"))
+    for c in root.findall("./bodies/body/components/c"):
+        used_components.add(c.get("d"))
 
-for file in target.with_suffix("").glob("*.bin"):
-    if not file.is_file():
-        continue
-    name = file.with_suffix("").name
-    if name not in used_components:
-        if args.remove_file:
-            print(f"{name}.bin is not used, removing file")
-            os.remove(file)
-        else:
-            print(f"{name}.bin is not used")
+    for file in target.with_suffix("").glob("*.bin"):
+        if not file.is_file():
+            continue
+        name = file.with_suffix("").name
+        if name not in used_components:
+            if remove_file:
+                if verbose:
+                    print(f"{name}.bin is not used, removing file")
+                os.remove(file)
+            else:
+                if verbose:
+                    print(f"{name}.bin is not used")
+                unused_components.add(name)
+
+    return unused_components
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("target", type=str)
+    parser.add_argument("-r", "--remove-file", action="store_true")
+    args = parser.parse_args()
+
+    detect_unused_components(Path(args.target), remove_file=True, verbose=True)
--- dev.py
+++ dev.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import shutil
 import subprocess
+from detect_unused_components import detect_unused_components
 from lib.load_env import load_env
 
 TARGET_FOLDERS = ["chuso3000"]
@@ -10,16 +11,49 @@ vehicles_path = Path(env["VEHICLES_PATH"])
 root_path = Path.cwd()
 dist_path = root_path.joinpath("dist")
 
+
+def copy_if_newer(source: Path, target: Path):
+    if not source.is_file():
+        return
+    if not target.is_file() or target.stat().st_mtime < source.stat().st_mtime:
+        shutil.copy(source, target)
+
+
 for folder in TARGET_FOLDERS:
     for t_file in root_path.joinpath(folder).glob("*.xml"):
         if not t_file.is_file():
             continue
         name = t_file.name
         v_file = vehicles_path.joinpath(name)
+        if not v_file.is_file():
+            continue
+
+        copy_if_newer(v_file, t_file)
+
+        t_folder = t_file.with_suffix("")
+        v_folder = v_file.with_suffix("")
+        if v_folder.is_dir():
+            for v_component in v_folder.glob("*.bin"):
+                if not v_component.is_file():
+                    continue
+                t_component = t_folder.joinpath(v_component.name)
+                copy_if_newer(v_component, t_component)
+
+        detect_unused_components(t_file, remove_file=True, verbose=True)
+
         if v_file.is_file() and v_file.stat().st_mtime > t_file.stat().st_mtime:
             print(
                 f'Copying "{name}" from "{vehicles_path.name}" to "{folder}"')
             shutil.copy(v_file, t_file)
+
+            print(v_folder, v_folder.is_dir())
+            if v_folder.is_dir():
+                print("copy dir")
+                shutil.copytree(
+                    v_folder,
+                    t_file.with_suffix(""),
+                    dirs_exist_ok=True
+                )
 
 shutil.rmtree(dist_path)
 
